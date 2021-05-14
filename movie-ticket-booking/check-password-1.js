a6cff91b7615ea208add66a17f3502b64875635b
sinduja0123 <51831780+sinduja0123@users.noreply.github.com>
2021-05-14 12:04:00 +0530

    Add files via upload

---
+++ movie-ticket-booking/check-password-1.js
@@ -0,0 +1,22 @@
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if(username == ""){
+    alert("Please enter a User Name")
+    formLogin.username.focus()
+    return false
+}
+if(password == ""){
+    alert("Please enter a Password")
+    formLogin.password.focus()
+    return false
+}
+if( username == "Test" && password == "test#123"){
+    alert("Login successfully");
+    window.location = "gameboard.html";
+    return false;
+}
+else{
+ alert("Login failed - Please enter correct Username and Password")   
+}
+}
