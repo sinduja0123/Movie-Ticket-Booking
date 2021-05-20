18fa3527c5fcc91291c32d42748f68f59de1d169
sinduja0123 <51831780+sinduja0123@users.noreply.github.com>
2021-05-20 10:33:16 +0530

    Update insert.php

--- movie-ticket-booking/insert.php
+++ movie-ticket-booking/insert.php
@@ -1,58 +1,25 @@
-<?php
-if (isset($_POST['submit'])) {
-    if (isset($_POST['username']) && isset($_POST['password'])) {
-        
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        
-
-        $host = "localhost";
-        $dbUsername = "root";
-        $dbPassword = "";
-        $dbName = "test";
-
-        $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
-
-        if ($conn->connect_error) {
-            die('Could not connect to the database.');
-        }
-        else {
-            $Select = "SELECT email FROM register WHERE email = ? LIMIT 1";
-            $Insert = "INSERT INTO register(username, password) values(?, ?)";
-
-            $stmt = $conn->prepare($Select);
-            $stmt->bind_param("s", $email);
-            $stmt->execute();
-            $stmt->bind_result($resultEmail);
-            $stmt->store_result();
-            $stmt->fetch();
-            $rnum = $stmt->num_rows;
-
-            if ($rnum == 0) {
-                $stmt->close();
-
-                $stmt = $conn->prepare($Insert);
-                $stmt->bind_param("ssssii",$username, $password);
-                if ($stmt->execute()) {
-                    echo "New record inserted sucessfully.";
-                }
-                else {
-                    echo $stmt->error;
-                }
-            }
-            else {
-                echo "Someone already registers using this email.";
-            }
-            $stmt->close();
-            $conn->close();
-        }
-    }
-    else {
-        echo "All field are required.";
-        die();
-    }
-}
-else {
-    echo "Submit button is not set";
-}
+$username = "anychart_user";
+  $password = "password";  
+  $host = "localhost";
+  $database="local_db";
+ 
+  
+  $server = mysql_connect($host, $username, $password);
+  $connection = mysql_select_db($database, $server);
+ 
+ 
+  $myquery = "
+    SELECT  * FROM  `data`
+    ";
+  $query = mysql_query($myquery);
+ 
+  if ( ! $query ) {
+    echo mysql_error();
+    die;
+  }
+ 
+  echo json_encode($data);  
+ 
+ 
+  mysql_close($server);
 ?>
